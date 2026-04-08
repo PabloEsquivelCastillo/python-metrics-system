f72db26e33285abaaa683cd84a1244e406d9b66e
Daniel-Aguilar-Dev <20233tn088@utez.edu.mx>
2026-04-08 12:19:39 -0600

    [FEAT] Token personalizado para incluir el tipo de rol

--- backend/usuarios/serializers.py
+++ backend/usuarios/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 # Esto obtiene dinámicamente tu modelo 'MiUsuario' gracias al settings.py
 User = get_user_model() 
@@ -11,6 +12,17 @@ class RegistroSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
+        validated_data['role'] = User.Role.CLIENT # Asignamos el rol CLIENT por defecto al crear un usuario
         # Como programamos el Manager arriba, create_user encriptará el password
         user = User.objects.create_user(**validated_data)
         return user
+
+# Personalizamos el serializer de JWT para incluir información adicional en el token
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Agregar información adicional al token
+        token['email'] = user.email
+        token['role'] = user.role # Agregamos el rol al token para distinguir entre CLIENT y ADMIN
+        return token
